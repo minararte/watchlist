5e1e383b4a55a9dcf28922cc5e3716e7ec7664e3
minararte <160561665+minararte@users.noreply.github.com>
2024-02-27 13:50:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,8 @@ def add_movie():
         img = form.coverpage.data
         if img and allowed_file(img.filename):
             filename = secure_filename(img.filename)
-            with open(img, 'rb') as f:
+            img.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'rb') as f:
                 coverpage_bytes = f.read()
                 coverpage_bytes_encoded = base64.b64encode(coverpage_bytes).decode('utf-8')
             movie = (movieObject
